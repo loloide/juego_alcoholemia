7dd4b3f61f366dfc3a5ae49c76df37dd51bcfd0f
loloide <lorendlc2005@gmail.com>
2021-09-21 16:26:39 -0300

    export falta poner dockerfile

--- juego alcohol al volante/scenes/car.gd
+++
@@ -1,4 +0,0 @@
-extends Area2D
-var anim = ["1", "2", "3"] 
-func _ready():
-	$AnimatedSprite.animation = anim[randi() % anim.size()]
--- juego alcohol al volante/scenes/coming_car.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody2D
-var anim = ["1", "2", "3"]
-func _ready():
-	$AnimatedSprite.animation = anim[randi() % anim.size()]
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
